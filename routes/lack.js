718910f64bd1be4cec6292a28a1b99575da043ed
Oliver Wanzek <o.wanzek@ostfalia.de>
2023-05-11 17:27:59 +0200

    created route lack

---
+++ routes/lack.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const fs = require('fs');
+const lacksFilePath = './data/lacks.json';
+
+
+//API Endpoints here
+
+
+module.exports = router;

bd222cae287cafe9678f5d95b0cfd194762a8ae1
Oliver Wanzek <o.wanzek@ostfalia.de>
2023-05-11 17:28:05 +0200

    created route vote

---
+++ routes/vote.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const fs = require('fs');
+const votesFilePath = './data/votes.json';
+
+
+
+//API Endpoints here
+
+
+module.exports = router;

7f29e66f1336027284455ea29077a6d3b9c6a5ab
Oliver Wanzek <o.wanzek@ostfalia.de>
2023-05-16 22:31:38 +0200

    created tokens.js class to generate all kind of tokens

---
+++ funcs/tokens.js
@@ -0,0 +1,59 @@
+
+/**
+ * Generates a random share token.
+ *
+ * @returns {string} The randomly generated share token
+ */
+function generateShareToken() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const tokenLength = 15;
+    let token = 'S-';
+
+    for (let i = 0; i < tokenLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters.charAt(randomIndex);
+    }
+    return token;
+}
+
+/**
+ * Generates a random admin token.
+ *
+ * @returns {string} The randomly generated admin token
+ */
+function generateAdminToken() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const tokenLength = 15;
+    let token = 'A-';
+
+    for (let i = 0; i < tokenLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters.charAt(randomIndex);
+    }
+
+    return token;
+}
+
+/**
+ * Generates a random edit token.
+ *
+ * @returns {string} The randomly generated edit token
+ */
+function generateEditToken() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const tokenLength = 15;
+    let token = 'E-';
+
+    for (let i = 0; i < tokenLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters.charAt(randomIndex);
+    }
+
+    return token;
+}
+
+module.exports = {
+    generateEditToken,
+    generateShareToken,
+    generateAdminToken
+};
